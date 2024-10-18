84ebeb1b5edd8092970e5698f5b7c26725dd32a6
KyleHaleYeah <kylehaleyeah@gmail.com>
2024-10-18 16:38:50 -0700

    added the shot method to the game.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shoot import Shot
 import pygame
 import sys
 
@@ -16,14 +17,17 @@ def main():
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     
-    Player.containers = (updateable, drawable)    
+    Player.containers = (updateable, drawable, shots)    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     Asteroid.containers = (asteroids, updateable, drawable)
 
     AsteroidField.containers = (updateable,)
     asteroid_field = AsteroidField()
+
+    Shot.containers = (updateable, drawable, shots)
 
     while True:
         for event in pygame.event.get():
--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 from CircleShape import CircleShape
 from constants import *
+from shoot import Shot
 import pygame
 
 class Player(CircleShape):
@@ -7,7 +8,6 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
 
-    # in the player class
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -33,7 +33,13 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        new_shot = Shot(self.position.x, self.position.y)
+        new_shot.velocity = pygame.Vector2(0, -1).rotate(-self.rotation) * PLAYER_SHOOT_SPEED
