27440e0b652138c92542e4ef76cb2abbd005c143
KyleHaleYeah <kylehaleyeah@gmail.com>
2024-10-18 09:52:01 -0700

    Added groups for updateables and drawables.

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+    dt = 0  
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updateable, drawable)    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -17,9 +21,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
         screen.fill((0, 0, 0))
-        player.draw(screen)
+        
+        for obj in updateable:
+            obj.update(dt)
+        
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
